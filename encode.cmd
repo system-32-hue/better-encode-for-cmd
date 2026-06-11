b32b305bde05e166e3264aa504771b97e2499e16
system-32-hue <guilhermereisfadigas@gmail.com>
2026-06-11 13:22:04 -0300

    Update encode.cmd

--- encode.cmd
+++ encode.cmd
@@ -1,1 +1,21 @@
+@echo off
+setlocal
 
+set URL=https://raw.githubusercontent.com/system-32-hue/better-encode-for-cmd/refs/heads/main/encode.cmd
+set FILE=%TEMP%\encode.cmd
+
+echo Baixando...
+powershell -Command "Invoke-WebRequest '%URL%' -OutFile '%FILE%'"
+
+echo.
+echo Arquivo salvo em:
+echo %FILE%
+
+echo.
+certutil -hashfile "%FILE%" SHA256
+
+echo.
+choice /M "Executar o arquivo baixado"
+if errorlevel 2 goto :eof
+
+call "%FILE%"
