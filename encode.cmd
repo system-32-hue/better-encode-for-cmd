76c5f67bf75c7092625b3e3336dacaf9206639a7
system-32-hue <guilhermereisfadigas@gmail.com>
2026-06-11 13:37:48 -0300

    Update encode.cmd

--- encode.cmd
+++ encode.cmd
@@ -1,17 +1,1 @@
-@echo off
-setlocal EnableDelayedExpansion
-
-:loop
-set /p input=^
-
-for /f "delims=" %%A in ('powershell -NoProfile -Command ^
-"$s='%input%';" ^
-"if($s -match '^encode\((.*)\)$'){" ^
-"  ($matches[1].ToCharArray() ^| ForEach-Object {[int][char]$_}) -join '-'" ^
-"} elseif($s -match '^decode\((.*)\)$'){" ^
-"  $out=''; foreach($n in $matches[1].Split('-')){$out += [char][int]$n}; $out" ^
-"} else {" ^
-"  'Use: encode(texto) ou decode(numeros)'" ^
-"}"') do echo %%A
-
-goto loop
+doskey encode=powershell -NoProfile -Command "$args[0].ToCharArray() ^| %% {[int][char]$_} -join '-'" $*
